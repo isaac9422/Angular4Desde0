cddcddea1fb29da5777ee5af646033b852d91159
Isaac Palacios <ipalacios@cidenet.com.co>
2018-02-27 18:16:01 -0500

    Interpolación

---
+++ appCurso/src/app/ejm-interpolacion/ejm-interpolacion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ejm-interpolacion',
+  templateUrl: './ejm-interpolacion.component.html',
+  styleUrls: ['./ejm-interpolacion.component.css']
+})
+export class EjmInterpolacionComponent implements OnInit {
+	puntuacion:number = 9;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
