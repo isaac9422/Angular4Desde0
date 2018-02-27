a8d862ab967c39a2fd1ab5bbbeb961b8a3129e32
Isaac <isaac9422@gmail.com>
2018-02-27 00:50:30 -0500

    Data Binding

--- appCurso/src/app/app.module.ts
+++ appCurso/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FechaActualComponent } from './fecha-actual/fecha-actual.component';
+import { CopyrightComponent } from './copyright/copyright.component';
+import { EjmInterpolacionComponent } from './ejm-interpolacion/ejm-interpolacion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FechaActualComponent,
+    CopyrightComponent,
+    EjmInterpolacionComponent
   ],
   imports: [
     BrowserModule
