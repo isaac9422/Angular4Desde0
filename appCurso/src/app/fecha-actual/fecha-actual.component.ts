0fb9bbe9cce048306f99ba199ef84f87d0f35439
Isaac <isaac9422@gmail.com>
2018-03-16 07:59:57 -0500

    Property binding

--- appCurso/src/app/fecha-actual/fecha-actual.component.ts
+++ appCurso/src/app/fecha-actual/fecha-actual.component.ts
@@ -8,4 +8,9 @@ import { Component } from '@angular/core';
 export class FechaActualComponent {
 
     hoy: any = new Date();
+    constructor(){
+        setInterval(()=>{
+            this.hoy = new Date();
+        },1000)
+    }
 }
