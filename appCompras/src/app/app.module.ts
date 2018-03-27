d3b5ec82b967b319588dc1862fec0219771c8be2
Isaac Palacios <ipalacios@cidenet.com.co>
2018-03-26 19:25:19 -0500

    Formularios

--- appCompras/src/app/app.module.ts
+++ appCompras/src/app/app.module.ts
@@ -1,27 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule} from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
 import { ProveedoresService } from './servicios/proveedores.service';
 import { ProveedoresComponent } from './proveedores/proveedores.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { HeaderComponent } from './header/header.component';
+import { AddproveeComponent } from './proveedores/addprovee/addprovee.component';
 
 const routes: Routes = [
   {path: '', component: InicioComponent},
-  {path: 'proveedores', component: ProveedoresComponent}
+  {path: 'proveedores', component: ProveedoresComponent},
+  {path: 'addProvee', component: AddproveeComponent}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     ProveedoresComponent,
-    InicioComponent
+    InicioComponent,
+    HeaderComponent,
+    AddproveeComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    FormsModule
   ],
   providers: [ProveedoresService],
   bootstrap: [AppComponent]
